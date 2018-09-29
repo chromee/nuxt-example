9243ff18687938f8778799636f38f1f0eb35141f
chromee <odk.mmmm@gmail.com>
2018-09-29 14:25:55 +0900

    storeまで

---
+++ pages/users/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{user.name}}</h1>
+    <img :src="user.avatar_url">
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  async asyncData({ params, store }) {
+    const { id } = params
+    await store.dispatch('getUser', { id })
+    // const user = await app.$axios.$get(`https://api.github.com/users/${id}`)
+    return {}
+  },
+  computed: {
+    ...mapGetters(["user"])
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <a href="http://localhost:3000/users/chromee">http://localhost:3000/users/chromee</a>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    const { id } = params
+    return { id }
+  }
+}
+</script>
